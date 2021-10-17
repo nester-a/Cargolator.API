866094c641a0c1ab7e7c26c8449467519c71d809
Aleksey <nester.a.l@yandex.ru>
2021-10-17 14:59:57 +0300

    Создание сущности контейнера

---
+++ Cargolator.Domain/Base/Container.cs
@@ -0,0 +1,18 @@
+﻿using Cargolator.Domain.Base.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cargolator.Domain.Base
+{
+    public class Container : ILoadable
+    {
+        public Stack<ICargo> LoadedCargo { get; set; } = new Stack<ICargo>();
+
+        public int Length { get; set; }
+
+        public int Width { get; set; }
+    }
+}
