240d1c6f131efef40c046c9eb947d810b5861131
Aleksey <nester.a.l@yandex.ru>
2021-10-17 14:51:55 +0300

    Создание сущности точки и координат

---
+++ Cargolator.Domain/Base/Interfaces/ICoordinates.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cargolator.Domain.Base.Interfaces
+{
+    public interface ICoordinates
+    {
+        IPoint UpperLeftCorner { get; }
+        IPoint LowerRightCorner { get; }
+    }
+}
---
+++ Cargolator.Domain/Base/Point.cs
@@ -0,0 +1,15 @@
+﻿using Cargolator.Domain.Base.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cargolator.Domain.Base
+{
+    public class Point : IPoint
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
