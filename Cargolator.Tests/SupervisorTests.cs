aac994f2c338d7e16052d66e26216dee8428cc0e
Aleksey <nester.a.l@yandex.ru>
2021-10-23 19:49:02 +0300

    Заготовка тест для поиска места

--- Cargolator.Tests/SupervisorTests.cs
+++ Cargolator.Tests/SupervisorTests.cs
@@ -3,10 +3,10 @@ using Xunit;
 
 namespace Cargolator.Tests
 {
-    public class UnitTest1
+    public class SupervisorTests
     {
         [Fact]
-        public void Test1()
+        public void FindLoadPlaceResult()
         {
 
         }
