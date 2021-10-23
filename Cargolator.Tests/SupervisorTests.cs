7d5f33d654b0bf5d74652ff05273fe814bfb54b1
Aleksey <nester.a.l@yandex.ru>
2021-10-23 20:00:26 +0300

    Добавление в тест блоков Arrange и Act. Блока Assert пока нет

--- Cargolator.Tests/SupervisorTests.cs
+++ Cargolator.Tests/SupervisorTests.cs
@@ -1,3 +1,4 @@
+using Cargolator.Domain.Base;
 using System;
 using Xunit;
 
@@ -8,7 +9,15 @@ namespace Cargolator.Tests
         [Fact]
         public void FindLoadPlaceResult()
         {
+            // Arrange
+            Container cnt = new Container() { Length = 10, Width = 10 };
+            Supervisor sv = new Supervisor(cnt);
+            Cargo crg = new Cargo() { Length = 5, Width = 2 };
 
+            // Act
+            Coordinates result = sv.FindLoadPlace(crg);
+
+            // Assert
         }
     }
 }
