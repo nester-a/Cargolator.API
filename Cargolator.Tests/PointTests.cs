4db5c9cd554bb08424e724cab55f17ed57dacc46
Aleksey <nester.a.l@yandex.ru>
2021-10-23 20:41:06 +0300

    Создание теста проверки равенства точек

---
+++ Cargolator.Tests/PointTests.cs
@@ -0,0 +1,27 @@
+﻿using Cargolator.Domain.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Cargolator.Tests
+{
+    public class PointTests
+    {
+        [Fact]
+        public void PointEqualsResult()
+        {
+            // Arrange
+            Point a = new Point() { X=4, Y=5};
+            Point b = new Point() { X=5, Y=5};
+
+            // Act
+            bool result = a.Equals(b);
+
+            // Assert
+            Assert.True(result);
+        }
+    }
+}
