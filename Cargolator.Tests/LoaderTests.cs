05ae81767662a49a6e50eaef59360aa33d10f88c
Aleksey <nester.a.l@yandex.ru>
2021-11-15 21:44:50 +0300

    Добавление пропущенных событий в класс Загрузчик. Создание заготовки тестов Загрузчика

---
+++ Cargolator.Tests/LoaderTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Cargolator.Tests
+{
+    public class LoaderTests
+    {
+        [Fact]
+        public void Test()
+        {
+
+        }
+    }
+}
