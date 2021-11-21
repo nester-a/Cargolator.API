cc98baa1b0ec96724d6efe9550be03d01f33d0bb
Aleksey <nester.a.l@yandex.ru>
2021-11-21 20:14:06 +0300

    Тесты сущности Склад

---
+++ Cargolator.Tests/StockTests.cs
@@ -0,0 +1,66 @@
+﻿using Cargolator.API.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Cargolator.Tests
+{
+    public class StockTests
+    {
+        [Fact]
+        public void AddOnStockTest()
+        {
+            // Arrange
+            Stock stck = new Stock();
+            Cargo crg = new Cargo(0, 1, 1);
+
+            // Act
+            stck.AddOnStock(crg);
+
+            bool result = stck.CargosStock.Count == 1 && stck.CargosStock.Contains(crg);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void AddRangeOnStockArrayTest()
+        {
+            // Arrange
+            Stock stck = new Stock();
+            Cargo crg1 = new Cargo(0, 1, 1);
+            Cargo crg2 = new Cargo(1, 2, 2);
+
+            // Act
+            stck.AddRangeOnStock(crg1, crg2);
+
+            bool result = stck.CargosStock.Count == 2 && stck.CargosStock.Contains(crg1) && stck.CargosStock.Contains(crg2);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void AddRangeOnStockCollectionTest()
+        {
+            // Arrange
+            Stock stck = new Stock();
+            List<Cargo> crgs = new List<Cargo>()
+            {
+                new Cargo(0, 1, 1),
+                new Cargo(1, 2, 2),
+            };
+
+            // Act
+            stck.AddRangeOnStock(crgs);
+
+            bool result = stck.CargosStock.Count == 2 && stck.CargosStock.Contains(new Cargo(0, 1, 1)) && stck.CargosStock.Contains(new Cargo(1, 2, 2));
+
+            // Assert
+            Assert.True(result);
+        }
+    }
+}
