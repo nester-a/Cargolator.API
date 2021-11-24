1730827bf0b0a38db366c507dc994bb28c2d0859
Aleksey <nester.a.l@yandex.ru>
2021-11-24 18:55:05 +0300

    Тестирование исключений конструктора сущности Точка

---
+++ Cargolator.Tests/PointExceptionsTests.cs
@@ -0,0 +1,59 @@
+﻿using Cargolator.API.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Cargolator.Tests
+{
+    public class PointExceptionsTests
+    {
+        [Fact]
+        public void PointConstructorXArgumentExceptionTest()
+        {
+            // Arrange
+
+            bool catched = false;
+
+            // Act
+
+            try
+            {
+                Point p = new Point(-1, 0);
+            }
+            catch (ArgumentException e)
+            {
+                if (e is not null) catched = true;
+            }
+
+            // Assert
+
+            Assert.True(catched);
+        }
+
+        [Fact]
+        public void PointConstructorYArgumentExceptionTest()
+        {
+            // Arrange
+
+            bool catched = false;
+
+            // Act
+
+            try
+            {
+                Point p = new Point(0, -1);
+            }
+            catch (ArgumentException e)
+            {
+                if (e is not null) catched = true;
+            }
+
+            // Assert
+
+            Assert.True(catched);
+        }
+    }
+}
