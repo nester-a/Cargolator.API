76523be5d81a9e67a83bd5f476e323a3372635d9
Aleksey <nester.a.l@yandex.ru>
2021-11-24 18:26:53 +0300

    Тестирование исключений конструктора сущности Координаты

---
+++ Cargolator.Tests/CoordinatesExceptionsTests.cs
@@ -0,0 +1,53 @@
+﻿using Cargolator.API.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Cargolator.Tests
+{
+    public class CoordinatesExceptionsTests
+    {
+        [Fact]
+        public void CoordinatesConstructorUpperLeftCornerArgumentNullExceptionTest()
+        {
+            // Arrange
+            bool catched = false;
+
+            // Act
+            try
+            {
+                Coordinates crg = new Coordinates(null, new Point(0, 0));
+            }
+            catch (ArgumentNullException e)
+            {
+                if (e is not null) catched = true;
+            }
+
+            // Assert
+            Assert.True(catched);
+        }
+
+        [Fact]
+        public void CoordinatesConstructorLowerRightCornerArgumentNullExceptionTest()
+        {
+            // Arrange
+            bool catched = false;
+
+            // Act
+            try
+            {
+                Coordinates crg = new Coordinates(new Point(0, 0), null);
+            }
+            catch (ArgumentNullException e)
+            {
+                if (e is not null) catched = true;
+            }
+
+            // Assert
+            Assert.True(catched);
+        }
+    }
+}
