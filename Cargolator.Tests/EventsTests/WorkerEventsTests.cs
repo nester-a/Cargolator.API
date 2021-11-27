3ae9dc257070df0124384d5a5daf643e72bccd6d
Aleksey <nester.a.l@yandex.ru>
2021-11-27 12:04:13 +0300

    Заготовка для тестирование сущности Рабочего

---
+++ Cargolator.Tests/EventsTests/WorkerEventsTests.cs
@@ -0,0 +1,52 @@
+﻿using Cargolator.API.Base.AbstractClasses;
+using Cargolator.API.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Cargolator.Tests.EventsTests
+{
+    public class WorkerEventsTests
+    {
+        bool testTrue = false;
+        bool testFalse = false;
+        string mes;
+
+        [Fact]
+        public void TakeMethodTakeCargoEventTest()
+        {
+            // Arrange
+            Worker wrk = new Loader();
+            Cargo crg = new Cargo(0, 1, 1);
+            wrk.TakeCargoEvent += Worker_TakeCargoEvent;
+
+            // Act
+            TestersReset();
+
+            // Assert
+        }
+
+        private void Worker_TakeCargoEvent(object sender, API.Base.EventArgs.WorkerEventArgs e)
+        {
+            if (e.EventResult == true)
+            {
+                testTrue = true;
+                mes = e.Message;
+            }
+            if(e.EventResult == false)
+            {
+                testFalse = true;
+                mes = e.Message;
+            }
+        }
+        private void TestersReset()
+        {
+            testTrue = false;
+            testFalse = false;
+            mes = null;
+        }
+    }
+}
