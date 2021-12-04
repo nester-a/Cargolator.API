edc1a80290cf7577afdbec34ad1fa779081512d5
Aleksey <nester.a.l@yandex.ru>
2021-12-04 22:47:31 +0300

    Удаление ненужных пространств имён в Аргументах Событий

--- Cargolator.Domain/Base/EventArgs/SupervisorEventArgs.cs
+++ Cargolator.Domain/Base/EventArgs/SupervisorEventArgs.cs
@@ -1,9 +1,4 @@
 ﻿using Cargolator.API.Base.AbstractClasses;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Cargolator.API.Base.EventArgs
 {
