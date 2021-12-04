db5c7f8ec5a0c292a3700895732fde35a74ac55d
Aleksey <nester.a.l@yandex.ru>
2021-12-04 22:50:27 +0300

    Удаление ненужных пространст имён из Интерфейсов

--- Cargolator.Domain/Base/Interfaces/ISupervisor.cs
+++ Cargolator.Domain/Base/Interfaces/ISupervisor.cs
@@ -1,9 +1,4 @@
-﻿using Cargolator.API.Base.AbstractClasses;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace Cargolator.API.Base.Interfaces
 {
